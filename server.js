feed052d45ec9867a55095aa45174fbb169de124
Balvant <bsbalvant2@gmail.com>
2024-10-15 18:23:25 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 8000
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS')
+  next();
+});
+
+
+
+app.get('/', async (req, res) => {
+  res.send('Welcome in aws server');  
+});
+
+app.use(function (err, req, res, next) {
+console.error("---",err.message)
+res.status(500).send('Something went wrong! '+err.message)
+});
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);  
+  });
